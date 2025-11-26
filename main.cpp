27bbfafe829d47a75512c64d0574845ac530a9d5
TommasoAngiuli <tommaso.angiuli@marconibari.edu.it>
2025-11-26 20:10:27 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+int massimo (int n1, int n2)
+{
+    int max;
+    if (n1>n2)
+    {
+        max=n1;
+    }
+    else
+    {
+        max=n2;
+    }
+    return max;
 }
-
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+int main() 
+{
+int n1, n2, n3, n4, max;
+cout<< "calcolo del massimo tra 4 numeri"<< endl;
+cout<< "inserisci 4 numeri"<< endl;
+cin>> n1;
+cin>>n2;
+cin>>n3;
+cin>>n4;
+max =massimo (n1,n2);
+max = massimo (max,n3);
+max = massimo (max, n4);
+cout<< "il massimo valore è " << max << endl;
+}
